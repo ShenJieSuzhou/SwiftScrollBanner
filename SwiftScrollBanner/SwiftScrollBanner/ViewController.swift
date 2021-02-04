158b704dbb7d2d9026544052d33d61d7151beddc
ShenJieSuzhou <745689122@qq.com>
2021-02-04 17:54:02 +0800

    效果1 Demo 代码上传
    效果1 Demo 代码上传

--- SwiftScrollBanner/SwiftScrollBanner/ViewController.swift
+++ SwiftScrollBanner/SwiftScrollBanner/ViewController.swift
@@ -9,55 +9,101 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var bannersData = [BannerModel]()
+//    var bannersData = [BannerModel]()
+//
+//    var privateData = [PrivateCustomModel]()
+//
+//    lazy var adScrollerBanner: JJNewsBanner = {
+//        let banner = JJNewsBanner(frame: CGRect(x: 0, y: 50, width: self.view.frame.size.width, height: 200))
+//        banner.currentPageDotColor = UIColor.white
+//        banner.pageDotColor = UIColor.gray
+//        banner.autoScrollTimeInterval = 5.0
+//        banner.pageControlDotSize = CGSize(width: 10, height: 10)
+//
+//        return banner
+//    }()
+//
+//    lazy var privateSongListView: PrivateCustomView = {
+//        let view = PrivateCustomView(frame: CGRect(x: 0, y: 200, width: self.view.frame.size.width, height: 300), data: privateData)
+//        return view
+//    }()
     
-    var privateData = [PrivateCustomModel]()
     
-    lazy var adScrollerBanner: JJNewsBanner = {
-        let banner = JJNewsBanner(frame: CGRect(x: 0, y: 50, width: self.view.frame.size.width, height: 200))
-        banner.currentPageDotColor = UIColor.white
-        banner.pageDotColor = UIColor.gray
-        banner.autoScrollTimeInterval = 5.0
-        banner.pageControlDotSize = CGSize(width: 10, height: 10)
-        
-        return banner
-    }()
+    private var tableList: UITableView!
     
-    lazy var privateSongListView: PrivateCustomView = {
-        let view = PrivateCustomView(frame: CGRect(x: 0, y: 200, width: self.view.frame.size.width, height: 300), data: privateData)
-        return view
-    }()
-    
+    private var simpleCubeView: SimpleCubeViewController!
    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // +++++++++++++++++++++++++++++ 轮播图 ++++++++++++++++++++++++++++++++++++++
-        self.view.addSubview(adScrollerBanner)
-
-        let model1: BannerModel = BannerModel(pic: "https://p1.music.126.net/g7O3fXseRtDKfiqLpQSV-g==/109951165439360647.jpg")
-        let model2: BannerModel = BannerModel(pic: "https://p1.music.126.net/TW3OuCmEZfxI6EWAJjp0Kg==/109951165626512352.jpg")
-        let model3: BannerModel = BannerModel(pic: "https://p1.music.126.net/Q9MXwiNR8oKuR_Zd5pncJw==/109951165043282575.jpg")
-        let model4: BannerModel = BannerModel(pic: "https://p1.music.126.net/3xjiLJOe4QflowFMm2Of4Q==/109951165636689592.jpg")
-        let model5: BannerModel = BannerModel(pic: "https://p1.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg")
-        let model6: BannerModel = BannerModel(pic: "https://p1.music.126.net/JqcjsT2bqtLqnpe8W7didg==/109951163680713819.jpg")
-        let model7: BannerModel = BannerModel(pic: "https://p1.music.126.net/IHbAYpNSMTvGt0oLS3QMUw==/109951165584948265.jpg")
-        let model8: BannerModel = BannerModel(pic: "https://p1.music.126.net/bfjxpm2L1JLAP3S02Jwduw==/109951165634942723.jpg")
-
-        adScrollerBanner.updateUI(imageUrlStrArray: [model1,model2,model3,model4,model5,model6,model7,model8], placeholderImage: UIImage(named: "ad_placeholder"))
+        tableList = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height), style: .plain)
+        tableList.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
+        tableList.delegate = self
+        tableList.dataSource = self
+        self.view.addSubview(tableList)
         
-        // +++++++++++++++++++++++++++++ 分页 ++++++++++++++++++++++++++++++++++++++
-        let songMode1: SongModel = SongModel(image: "http://p2.music.126.net/1gNcBmzdIaQtU00Dvp_TvQ==/109951163912081772.jpg", order: 1, songName: "一路向北", singer: "周杰伦", extra: "新")
-        let songMode2: SongModel = SongModel(image: "http://p2.music.126.net/1gNcBmzdIaQtU00Dvp_TvQ==/109951163912081772.jpg", order: 1, songName: "一路向北", singer: "周杰伦", extra: "新")
-        let songMode3: SongModel = SongModel(image: "http://p2.music.126.net/1gNcBmzdIaQtU00Dvp_TvQ==/109951163912081772.jpg", order: 1, songName: "一路向北", singer: "周杰伦", extra: "新")
         
-        // 刷新
-        let songs: [SongModel] = [songMode1, songMode2, songMode3]
-        let privateModel: PrivateCustomModel = PrivateCustomModel(songsList: songs)
-        privateData = [privateModel, privateModel, privateModel]
         
-        self.view.addSubview(privateSongListView)
+//        // +++++++++++++++++++++++++++++ 轮播图 ++++++++++++++++++++++++++++++++++++++
+//        self.view.addSubview(adScrollerBanner)
+//
+//        let model1: BannerModel = BannerModel(pic: "https://p1.music.126.net/g7O3fXseRtDKfiqLpQSV-g==/109951165439360647.jpg")
+//        let model2: BannerModel = BannerModel(pic: "https://p1.music.126.net/TW3OuCmEZfxI6EWAJjp0Kg==/109951165626512352.jpg")
+//        let model3: BannerModel = BannerModel(pic: "https://p1.music.126.net/Q9MXwiNR8oKuR_Zd5pncJw==/109951165043282575.jpg")
+//        let model4: BannerModel = BannerModel(pic: "https://p1.music.126.net/3xjiLJOe4QflowFMm2Of4Q==/109951165636689592.jpg")
+//        let model5: BannerModel = BannerModel(pic: "https://p1.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg")
+//        let model6: BannerModel = BannerModel(pic: "https://p1.music.126.net/JqcjsT2bqtLqnpe8W7didg==/109951163680713819.jpg")
+//        let model7: BannerModel = BannerModel(pic: "https://p1.music.126.net/IHbAYpNSMTvGt0oLS3QMUw==/109951165584948265.jpg")
+//        let model8: BannerModel = BannerModel(pic: "https://p1.music.126.net/bfjxpm2L1JLAP3S02Jwduw==/109951165634942723.jpg")
+//
+//        adScrollerBanner.updateUI(imageUrlStrArray: [model1,model2,model3,model4,model5,model6,model7,model8], placeholderImage: UIImage(named: "ad_placeholder"))
+//
+//        // +++++++++++++++++++++++++++++ 分页 ++++++++++++++++++++++++++++++++++++++
+//        let songMode1: SongModel = SongModel(image: "http://p2.music.126.net/1gNcBmzdIaQtU00Dvp_TvQ==/109951163912081772.jpg", order: 1, songName: "一路向北", singer: "周杰伦", extra: "新")
+//        let songMode2: SongModel = SongModel(image: "http://p2.music.126.net/1gNcBmzdIaQtU00Dvp_TvQ==/109951163912081772.jpg", order: 1, songName: "一路向北", singer: "周杰伦", extra: "新")
+//        let songMode3: SongModel = SongModel(image: "http://p2.music.126.net/1gNcBmzdIaQtU00Dvp_TvQ==/109951163912081772.jpg", order: 1, songName: "一路向北", singer: "周杰伦", extra: "新")
+//
+//        // 刷新
+//        let songs: [SongModel] = [songMode1, songMode2, songMode3]
+//        let privateModel: PrivateCustomModel = PrivateCustomModel(songsList: songs)
+//        privateData = [privateModel, privateModel, privateModel]
+//
+//        self.view.addSubview(privateSongListView)
     }
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (indexPath.row == 0) {
+            simpleCubeView = SimpleCubeViewController()
+            self.present(simpleCubeView, animated: true) {
+                
+            }
+            //self.navigationController?.pushViewController(simpleCubeView, animated: true)
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
+        if (indexPath.row == 0){
+            cell.textLabel?.text = "效果1"
+        } else if (indexPath.row == 1){
+            cell.textLabel?.text = "效果2"
+        } else {
+            cell.textLabel?.text = "其他"
+        }
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
---
+++ SwiftScrollBanner/SwiftScrollBanner/ViewController/SimpleCubeViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SimpleCubeViewController.swift
+//  SwiftScrollBanner
+//
+//  Created by shenjie on 2021/2/4.
+//
+
+import UIKit
+
+class SimpleCubeViewController: UIViewController {
+    
+    private var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.bounds = UIScreen.main.bounds
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height), collectionViewLayout: flowLayout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellID")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        self.view.addSubview(collectionView)
+    }
+}
+
+extension SimpleCubeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if (indexPath.row % 2 == 0) {
+            return CGSize(width: 50, height: 50)
+        } else {
+            return CGSize(width: 100, height: 100)
+        }
+    }
+}
+
+extension SimpleCubeViewController: UICollectionViewDelegate {
+    
+}
+
+extension SimpleCubeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellID", for: indexPath)
+        cell.backgroundColor = UIColor(red: 10/255, green: 10/255, blue: 10/255, alpha: 1)
+        return cell
+    }
+}
+
