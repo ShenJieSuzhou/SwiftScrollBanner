2bb8b4e661ad6a124def2b96139905f571258794
ShenJieSuzhou <745689122@qq.com>
2021-03-03 10:37:44 +0800

    UICollectionView 装饰视图调研完成
    UICollectionView 装饰视图调研完成

--- SwiftScrollBanner/SwiftScrollBanner/Layout/BaseAPIFlowLayout.swift
+++ SwiftScrollBanner/SwiftScrollBanner/Layout/BaseAPIFlowLayout.swift
@@ -20,31 +20,28 @@ class BaseAPIFlowLayout: UICollectionViewFlowLayout {
     
 
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        var attributes = super.layoutAttributesForElements(in: rect)
-//        var newAttrs = [UICollectionViewLayoutAttributes]()
-//        attributes!.forEach({ (attr) in
-//            if attr.representedElementKind == UICollectionView.elementKindSectionHeader {
-//                let attr1 = layoutAttributesForSupplementaryView(ofKind:UICollectionView.elementKindSectionHeader, at: IndexPath(item: 0, section: 0))
-//                newAttrs.append(attr1!)
-//            } else if attr.representedElementKind == UICollectionView.elementKindSectionFooter {
-//                newAttrs.append(attr)
-//            }
-//
-//            if attr.representedElementKind == nil {
-//                attr.transform = CGAffineTrattributesansform(rotationAngle: 45)
-//                newAttrs.append(attr)
-//            }
-//
-//            if attr.representedElementCategory == .decorationView {
-//                let decorAttr = self.layoutAttributesForDecorationView(ofKind: "DecorationView", at: IndexPath(item: 0, section: 0))
-//                newAttrs.append(decorAttr!)
-//            }
-//        })
+        let attributes = super.layoutAttributesForElements(in: rect)
+        var newAttrs = [UICollectionViewLayoutAttributes]()
+        attributes!.forEach({ (attr) in
+            if attr.representedElementKind == UICollectionView.elementKindSectionHeader {
+                let attr1 = layoutAttributesForSupplementaryView(ofKind:UICollectionView.elementKindSectionHeader, at: IndexPath(item: 0, section: 0))
+                newAttrs.append(attr1!)
+            } else if attr.representedElementKind == UICollectionView.elementKindSectionFooter {
+                newAttrs.append(attr)
+            }
+
+            if attr.representedElementKind == nil {
+                attr.transform = CGAffineTransform(rotationAngle: 45)
+                newAttrs.append(attr)
+            }
+        })
+        
         let decorations = self.layoutAttributesForDecorationView(ofKind: "DecorationView", at: IndexPath(item: 0, section: 0))
         if let decorate = decorations, rect.intersects(decorate.frame){
-            attributes?.append(decorate)
-                }
-        return attributes;
+            newAttrs.append(decorate)
+        }
+    
+        return newAttrs;
     }
 
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
@@ -54,16 +51,12 @@ class BaseAPIFlowLayout: UICollectionViewFlowLayout {
     }
 
     override func layoutAttributesForDecorationView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        
-        var attributes = UICollectionViewLayoutAttributes(forDecorationViewOfKind: "DecorationView", with: indexPath)
+        let attributes = UICollectionViewLayoutAttributes(forDecorationViewOfKind: "DecorationView", with: indexPath)
        
-        attributes.frame = CGRect(x: 0, y: 0, width: CGFloat(collectionView!.frame.size.width), height: 500)
+        attributes.frame = CGRect(x: 0, y: 0, width: CGFloat(collectionView!.frame.size.width), height: 1000)
         attributes.zIndex = -1
 
-            return attributes
-//        }
-//
-//        return attributes = super.layoutAttributesForDecorationView(ofKind: elementKind, at: indexPath)
+        return attributes
     }
 
     override func layoutAttributesForSupplementaryView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
